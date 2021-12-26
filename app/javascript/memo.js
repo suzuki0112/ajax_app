b1b0ae179fa5b0af12d8fea80fbe13b3b5f516a1
suzuki0112 <banana.is.justice@gmail.com>
2021-12-26 18:54:50 +0900

    コントローラーにリクエストを送信する

---
+++ app/javascript/memo.js
@@ -0,0 +1,15 @@
+function post (){
+  console.log("ok")
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+  });
+};
+
+window.addEventListener('load', post);
